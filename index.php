6f36bc551cea2d0a5463e56569c169e6e454beb3
drumattic00 <55857644+drumattic00@users.noreply.github.com>
2024-10-01 04:46:59 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require "inc/header.inc.php";
     <?php require "inc/init_pop.inc.php"; ?>
 
     <div class="container">
-        <h1>Use Ajax in PHP</h1>
+        <h1>Picks Against The Spread</h1>
 
         <div class="table-responsive">
             <h2>NFL Week <?= $week ?></h2>
